b3eb27f54eb5b94bc20ea00eb9672b1d857d6f21
Tadeo hepperle <tadeo@do-mix.de>
2025-11-06 05:47:49 +0700

    add Zig wrapper package around Rust dynamic libraries

--- src/example.zig
+++ src/example.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 
 const sdffont = @import("sdffont");
+const SdfFont = sdffont.SdfFont;
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
     const font_path = "odin_example/MarkoOne-Regular.default.ttf";
@@ -9,14 +10,19 @@ pub fn main() !void {
     const bytes = try std.fs.cwd().readFileAlloc(allocator, font_path, 10 * 1024 * 1024);
     defer allocator.free(bytes);
 
-    const font = sdffont.font_create(bytes.ptr, bytes.len, sdffont.SDF_FONT_SETTINGS_DEFAULT, null);
-    if (font == null) {
-        std.debug.print("font_create failed\n", .{});
-        return;
-    }
+    const font = try SdfFont.create(bytes, sdffont.SDF_FONT_SETTINGS_DEFAULT);
+    defer font.free();
 
-    const metrics = sdffont.font_get_line_metrics(font);
+    const metrics = font.getLineMetrics();
     std.debug.print("ascent={}, descent={}\n", .{ metrics.ascent, metrics.descent });
 
-    sdffont.font_free(font);
+    std.debug.print("{}\n", .{font.hasAtlasImageChanged()});
+
+    std.debug.print("glyph for 'A': {}\n", .{font.getOrAddGlyph('A')});
+    std.debug.print("atlas_image_changed: {}\n", .{font.hasAtlasImageChanged()});
+    const img = font.getAtlasImage();
+    std.debug.print("atlas image size: {any}\n", .{img.size});
+
+    std.debug.print("glyph for ' ': {}\n", .{font.getOrAddGlyph(' ')});
+    std.debug.print("atlas_image_changed: {}\n", .{font.hasAtlasImageChanged()});
 }
--- src/root.zig
+++ src/root.zig
@@ -1,6 +1,4 @@
-// ---------- Type definitions ----------
-
-pub const SdfFont = ?*anyopaque;
+const std = @import("std");
 
 pub const SdfFontSettings = extern struct {
     font_size: u32,
@@ -41,24 +39,84 @@ pub const GlyphInfo = extern struct {
     uv_max: [2]f32,
 };
 
-pub const AtlasImage = extern struct {
+const AtlasImageRaw = extern struct {
     size: [2]u32,
-    bytes: [*]u8,
+    bytes: RawSlice,
 };
 
-// ---------- Foreign functions ----------
+/// Grayscale image of sdf font
+pub const AtlasImage = struct {
+    size: [2]u32,
+    bytes: []const u8,
+};
+
 // Use "sdffont" as the library name (links to libsdffont.so or sdffont.dll)
+const RawSlice = extern struct {
+    ptr: [*]const u8,
+    len: isize,
 
-pub extern "sdffont" fn font_create(
-    bytes: [*]const u8,
-    len: usize,
+    fn fromSlice(slice: []const u8) @This() {
+        return .{
+            .ptr = slice.ptr,
+            .len = @intCast(slice.len),
+        };
+    }
+
+    fn asSlice(self: @This()) []const u8 {
+        var err_string: []const u8 = undefined;
+        err_string.ptr = self.ptr;
+        err_string.len = @intCast(self.len);
+        return err_string;
+    }
+};
+const RawString = RawSlice;
+
+pub const SdfFont = opaque {
+    const Self = @This();
+    pub fn create(bytes: []const u8, settings: SdfFontSettings) !*Self {
+        var err: RawString = undefined;
+        const self = font_create(RawSlice.fromSlice(bytes), settings, &err) orelse {
+            std.log.err("could not load SdfFont: {s}", .{err.asSlice()});
+            return error.CouldNotCreateFont;
+        };
+        return self;
+    }
+    pub fn free(self: *Self) void {
+        font_free(self);
+    }
+    // Returns true if the atlas has changed (new glyphs added) since the last call to `getAtlasImage`.
+    // So calling getAtlasImage resets this to false. Can be used every frame to check if texture writes
+    // to the atlas texture need to be done. Currently only writing the entire texture is supported.
+    // In the future we should provide finer dirty regions that need to be uploaded to the gpu.
+    pub inline fn hasAtlasImageChanged(self: *Self) bool {
+        return font_has_atlas_image_changed(self);
+    }
+    pub inline fn getAtlasImage(self: *Self) AtlasImage {
+        const atlas_image_raw = font_get_atlas_image(self);
+        return AtlasImage{
+            .size = atlas_image_raw.size,
+            .bytes = atlas_image_raw.bytes.asSlice(),
+        };
+    }
+    pub inline fn getOrAddGlyph(self: *Self, codepoint: u32) GlyphInfo {
+        return font_get_or_add_glyph(self, codepoint);
+    }
+    pub inline fn getHorizontalKerning(self: *Self, left_codepoint: u32, right_codepoint: u32) f32 {
+        return font_get_horizontal_kerning(self, left_codepoint, right_codepoint);
+    }
+    pub inline fn getLineMetrics(self: *Self) LineMetrics {
+        return font_get_line_metrics(self);
+    }
+};
+
+extern "sdffont" fn font_create(
+    bytes: RawSlice,
     settings: SdfFontSettings,
-    err: ?*[*:0]const u8,
-) callconv(.c) SdfFont;
-
-pub extern "sdffont" fn font_free(font: SdfFont) callconv(.c) void;
-pub extern "sdffont" fn font_has_atlas_image_changed(font: SdfFont) callconv(.c) bool;
-pub extern "sdffont" fn font_get_atlas_image(font: SdfFont) callconv(.c) AtlasImage;
-pub extern "sdffont" fn font_get_or_add_glyph(font: SdfFont, ch: u32) callconv(.c) GlyphInfo;
-pub extern "sdffont" fn font_get_horizontal_kerning(font: SdfFont, left: u32, right: u32) callconv(.c) f32;
-pub extern "sdffont" fn font_get_line_metrics(font: SdfFont) callconv(.c) LineMetrics;
+    err: *RawString,
+) callconv(.c) ?*SdfFont;
+extern "sdffont" fn font_free(font: *SdfFont) callconv(.c) void;
+extern "sdffont" fn font_has_atlas_image_changed(font: *SdfFont) callconv(.c) bool;
+extern "sdffont" fn font_get_atlas_image(font: *SdfFont) callconv(.c) AtlasImageRaw;
+extern "sdffont" fn font_get_or_add_glyph(font: *SdfFont, ch: u32) callconv(.c) GlyphInfo;
+extern "sdffont" fn font_get_horizontal_kerning(font: *SdfFont, left: u32, right: u32) callconv(.c) f32;
+extern "sdffont" fn font_get_line_metrics(font: *SdfFont) callconv(.c) LineMetrics;
